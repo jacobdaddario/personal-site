382005bca28dcd03e0329a698b2ba0738b2161b6
HeyoRockbreaker <jake.daddario@gmail.com>
2018-06-28 23:56:32 -0500

    Finished configuration of packages. Added google font to layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
   end
 
   test "should get info" do
-    get static_pages_info_url
+    get "/info"
     assert_response :success
   end
 
