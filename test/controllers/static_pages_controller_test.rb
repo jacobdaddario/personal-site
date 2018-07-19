22c0656a03d8a25baed98fb82ca60f19cca1dd44
HeyoRockbreaker <jake.daddario@gmail.com>
2018-07-18 23:00:20 -0500

    Added rudimentary testing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,26 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Home - Jacob Daddario",
+    assert_select 'title', "Home - Jacob Daddario",
                   "Site title is not displaying correctly"
   end
 
   test "should get contact" do
-    get "/contact"
+    get contact_url
     assert_response :success
-    assert_select "title", "Contact - Jacob Daddario",
+    assert_select 'title', "Contact - Jacob Daddario",
                   "Site title is not displaying correctly"
   end
 
   test "should find author image" do
-    # TODO
+    get root_url
+    assert_select 'img.auth-image', nil,
+                  "Site not fetching author image"
   end
 
+  test "should find links to contact on home" do
+    get root_url
+    assert_select 'a[href=?]', '/contact', 4,
+                  "Contact links are missing"
+  end
 end
