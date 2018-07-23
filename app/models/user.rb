5af97eaeedf9f2cca8ab0a0f8a12ea6afcc0c35a
HeyoRockbreaker <jake.daddario@gmail.com>
2018-07-23 13:16:57 -0500

    Completed email authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
+
+  before_validation :downcase_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # Remember that uniqueness can have certain scopes set.
   # Don't forget to index the DB to also check for uniqnuess.
   # Also good for speed, but more important for big sites
   validates :name, presence: true, uniqueness: true, length: { maximum: 70 }
+  validates :email, presence: true, uniqueness: { case_sensative: false },
+                    length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+
+  private
+
+    def downcase_email
+      # DANGER, using bang on an already downcased email will return nil
+      self.email = email.to_s.downcase
+    end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   setup do
-    @base = User.create(name: "Joe")
+    @base = User.create(name: "Joe", email: "joe@gmail.com")
   end
 
   test "accepts a valid user" do
@@ -12,33 +12,67 @@ class UserTest < ActiveSupport::TestCase
   test "denies name already in use" do
     @base.save
     @new_base = @base.dup
-    assert_not @new_base.save, "App not forcing unique names"
+    @new_base.email = 'new@gmail.com'
+    assert_not @new_base.valid?, "App not forcing unique names"
   end
 
   test "denies name that is too long" do
-    assert @base.save, "App limiting name length too much"
-    @base = User.create(name: 'a' * 71)
-    assert_not @base.save, "App not limiting name length"
+    assert @base.valid?, "App limiting name length too much"
+    @base.name = 'a' * 71
+    assert_not @base.valid?, "App not limiting name length"
   end
 
   test "user without name is invalid" do
-    @base = User.create
-    assert_not @base.save, "App not validating user name presence"
-  end
-
-  test "denies invalid email" do
+    @base.name = nil
+    assert_not @base.valid?, "App not validating user name presence"
   end
 
   test "denies email already in use" do
+    @base.valid?
+    @new_base = @base.dup
+    @new_base.name = "Steve"
+    assert_not @new_base.valid?
   end
 
   test "denies email that is too long" do
+    @base.email = "a" * 245 + "@gmail.com"
+    assert @base.valid?, "App does not consider #{@base.email.length} characters legal"
+    @base.email = "a" * 246 + "@gmail.com"
+    assert_not @base.valid?, "App does not consider #{@base.email.length} characters too long"
   end
 
   test "user without email is invalid" do
+    @new_base = User.new(name: "Steve Failure")
+    assert_not @new_base.valid?, "App is not checking for user's email"
   end
 
+  test "email validation should accept valid addresses" do
+    # %w creates a double quote strings inside an array
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |address|
+      @base.email = address
+      assert @base.valid?, "#{address.inspect} should be valid"
+    end
+  end
+
+  test "denies invalid email" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |address|
+      @base.email = address
+      assert_not @base.valid?, "#{address.inspect} should be invalid"
+    end
+  end
+
+  # This is key to uniqueness actually functioning correctly
   test "changes the case of the email to lowercase" do
+    @base.save
+    @new_base = User.new(name: "Steve Failure", email: "Joe@gmail.com")
+    assert_not @new_base.valid?, "App not downcasing email"
+    @new_base = User.new(name: "Steve Double-check", email: "check@gmail.com")
+    assert_not @new_base.email == nil,
+               "App is changing downcased email to nil"
   end
 
   test "denies info that is too long" do
