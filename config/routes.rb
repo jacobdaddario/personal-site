05ab2ba0628bb77d57a787d5af58022ed6049807
HeyoRockbreaker <jake.daddario@gmail.com>
2018-07-26 11:47:13 -0500

    Generated a sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destry'
   root to: 'static_pages#home'
   get "/contact", to: 'static_pages#contact'
 
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get sessions_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get sessions_create_url
+    assert_response :success
+  end
+
+  test "should get destry" do
+    get sessions_destry_url
+    assert_response :success
+  end
+
+end
