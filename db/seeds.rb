2bc1b7d8c9dac255be1a59e17eaed9b76b4c6d53
HeyoRockbreaker <jake.daddario@gmail.com>
2018-09-01 15:46:02 -0500

    Re-wrote the seeding for the site

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-50.times { |i| Post.create!(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraphs(4), user_id: 1) }
+50.times {
+    Post.create!(
+    title: Faker::Lorem.sentence,
+    content: Faker::HarryPotter.quote,
+    user_id: 1
+    )
+}
