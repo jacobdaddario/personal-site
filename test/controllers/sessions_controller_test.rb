b8adbb2443941fc3be156aab22ad12592ee85c94
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-01 13:10:27 +0800

    Delete test directory

--- test/controllers/sessions_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-
-  # Don't have any functions checking if these pages give correct responses
-  # since the integration tests would catch this
-
-end
